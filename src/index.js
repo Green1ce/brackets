fe3a1b9a6d252bb55cfc042bd4514d1603976ae0
Green1ce <cha1iiep@mail.ru>
2021-03-04 22:38:56 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 != 0) return false;
+    let controllArr = [];
+    let arr1 = ['|', '(', '[', '{'];
+    let arr2 = ['|', ')', ']', '}'];
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < arr1.length; j++) {
+            if (str[i] == arr1[j]) controllArr.push(j);
+            else if (str[i] == arr2[j]) controllArr.push((j) * (-1));
+        }
+    }
+    for (let i = 0; i < controllArr.length; i++) {
+        if (controllArr[i + 1] < 0) {
+            if (controllArr[i] > 0 && (controllArr[i] * (-1)) == controllArr[i + 1]) {
+                controllArr.splice(i, 2);
+                i -= 2;
+            } else return false;
+        }
+    }
+    return true;
 }
